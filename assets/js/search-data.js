80419ad965239f792d4126a3245f0b7eb9c76c43
byeongtakpark <125178959+byeongtakpark@users.noreply.github.com>
2026-02-02 23:56:35 +0000

    Deploying to gh-pages from @ byeongtakpark/byeongtakpark.github.io@a570e4d993c7617405e25ca85624b350c41f140c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -21,7 +21,7 @@ ninja.data = [{
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/https:/drive.google.com/file/d/1wvyMiUWROnld1MuoovIV_Ag5wqcQZj_X/view?usp=sharing";
+            window.location.href = "/https:/drive.google.com/file/d/1PNXF5Bg74EVp5ZiivQporlE93xKnYdA4/view?usp=sharing";
           },
         },{id: "post-a-post-with-plotly-js",
         
